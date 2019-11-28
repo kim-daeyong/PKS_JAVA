02ebb9c2f789a672ab360f4f557f782520cbde28
coneseo <woon4754@gmail.com>
2019-11-28 23:51:55 +0900

    DAY3 Objects 클래스 설명

---
+++ src/main/java/spring/week1/DAY3.java
@@ -0,0 +1,10 @@
+package spring.week1;
+
+public class DAY3 {
+  /**
+   * Objects 클래스
+   * Object와 유사한 이름을 가진 java.util.Objects 클래스는 객체 비교, 해시코드 생성, null 여부,
+   * 객체 문자열 리턴 등의 연산을 수행하고 있는 정적 메소드들로 구성된 Object의 유틸리티 클래스이다.
+   */
+
+}
