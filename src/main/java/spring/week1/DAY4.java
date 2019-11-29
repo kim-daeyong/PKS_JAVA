60cd26a3fb4bf1825a8a711effca0ae096a8e99e
coneseo <woon4754@gmail.com>
2019-11-29 21:02:00 +0900

    DAY4 String 클래스 추가

---
+++ src/main/java/spring/week1/DAY4.java
@@ -0,0 +1,16 @@
+package spring.week1;
+
+public class DAY4 {
+/**
+ * String 클래스
+ * String 메소드
+ * 문자열의 추출, 비교, 찾기, 분리, 변환등과 같은 다양한 메소드를 가짐
+ * 사용빈도 높은 메소드
+ * charAt(int index), equals(Object object), getBytes(),
+ * toLowerCase(), toUpperCase(), replace("자바", "파이썬"),
+ * substring(), trim() : 앞뒤 공백 잘라내기
+ * valueOf() : 기본 타입의 값을 문자열로 변환
+ * split() : 정규 표현식을 구분자로 해서 부분 문자열 분리
+ * 배열에 저장 후 리턴
+ */
+}
