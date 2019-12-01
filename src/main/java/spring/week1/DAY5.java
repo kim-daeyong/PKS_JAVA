4fafd5e199e271b945b5faecd826f6de485904e2
coneseo <woon4754@gmail.com>
2019-12-01 23:47:19 +0900

    DAY5 stringbuilder 추가

--- src/main/java/spring/week1/DAY5.java
+++ src/main/java/spring/week1/DAY5.java
@@ -12,6 +12,25 @@ public class DAY5{
 
       //문자로 추출
       System.out.println(str1.charAt(3));
+
+      //StringBuilder
+      StringBuilder sb = new StringBuilder();
+      sb.append("java");
+      sb.append("program Study");
+      System.out.println(sb.toString());
+
+      //4번째 문자뒤에 2를 삽입
+      sb.insert(4,"2");
+
+      //문자를 6으로 변경
+//      sb.charAt(4,'6');
+
+      //문자를 대치
+      sb.replace(6,13, "Book");
+
+      String result = sb.toString();
+      System.out.println(result
+      );
     }
 
 }
