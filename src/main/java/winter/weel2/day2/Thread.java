a96b5ac0dcba3588dde073f6731ea98d778a8873
kim-daeyong <kdy2353@gmail.com>
2019-12-12 23:44:57 +0900

    thread

--- src/main/java/winter/weel2/day2/Thread.java
+++ src/main/java/winter/weel2/day2/Thread.java
@@ -9,7 +9,7 @@ public class Thread {
 
 //    Thread thread = new Thread(new Runnable() {
 //
-//
+//implements Rumnable
 //        public void run(){
 //
 //
@@ -21,7 +21,25 @@ public class Thread {
 //
 //
 //    });
-// 메인스레드 > 스레드 객체 생성 > start() 메소드 호충 > 메인 스레드 실행
+// 메인스레드 > 스레드 객체 생성 > start( 메소드 호충 > 메인 스레드 실행
 
     //thread.start();
+    //extends Thread
+    // @Override
+    //public void run(){
+    //}
+    /*
+    *Thread thread = new Thread(){
+    *   public void run(){
+    *   }
+    *
+    *  메소드
+    *  thread.setName
+    *  getName
+    *  setPriority(우선순위)
+    *  우선순위
+    *  Thread.MAX_PRIORITY - 10
+    *  .NORM_PRIORITY - 5
+    *  .MIN_PRIORITY - 1
+     */
 }
