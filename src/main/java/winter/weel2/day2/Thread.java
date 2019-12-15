4e765c70145df63e75137385232fed7bf76ba86f
kim-daeyong <kdy2353@gmail.com>
2019-12-15 23:26:30 +0900

    thread

--- src/main/java/winter/weel2/day2/Thread.java
+++ src/main/java/winter/weel2/day2/Thread.java
@@ -42,5 +42,20 @@ public class Thread {
     *  .NORM_PRIORITY - 5
     *  .MIN_PRIORITY - 1
     *  synchronized - 동기화 단하나의 스레드만 실행
+    *  .getState - 스레드 상태얻기
+    *  notify - tlfgodeorl
+    *  wait - 동기화블럭에서 일시정지
+    *  yield 양보
+    *  join - 스레드의 종료를 기다림
+    *  setDaemon - 주스레드의 보조, 주스레드가 종료되면 강제적으로 종료
+    *
+    *  스레드 객체 생성(NEW) - sㅅart - 실행 대기(RUNNABLE) / waiting ,timed wait, blocked <- 반복 -> 실행 - 종료(TERMINATED)
+    *
+    *  객체 생성(new) - start() 메소드가 호출되지않은 상태
+    *  실행 대기 - 실행 상태로 들어갈 수 있는 생태
+    *  waiting - 다른 스레드가 통지할 때까지 기다리는 상태
+    *  timed_wait - 주어진 시간 동안 기다리는 상태
+    *  blocked - 사용하고자 하는 객체의 락이 풀릴 때까지 기다리는 상태
+    *  종료 - 실행을 마친 상태
      */
 }
