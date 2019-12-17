41e81b10a065ecbca8103411af332bc13be187ff
coneseo <woon4754@gmail.com>
2019-12-17 23:53:41 +0900

    DAY8 BoxingUnBoxingExample Boxing

--- src/main/java/spring/week1/DAY8.java
+++ src/main/java/spring/week1/DAY8.java
@@ -2,4 +2,12 @@ package spring.week1;
 
 public class DAY8 {
 
+  public static void main(String[] args) {
+    //Boxing
+    Integer obj1 = new Integer(100);
+    Integer obj2 = new Integer(200);
+    Integer obj3 = new Integer.valueOf("300");
+
+
+  }
 }
